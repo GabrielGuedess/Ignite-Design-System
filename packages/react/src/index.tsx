37a772209394ed38077c070774340d21988a89f1
Gabriel Guedes <gabrielrguedess@gmail.com>
2023-02-07 15:43:14 -0300

    feat: add turbo repo

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from './styles';
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  background: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+  border: 0,
+});
